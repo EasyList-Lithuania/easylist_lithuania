43b77f2474bfb8770cc48f6eb384a06f2d11ec96
gymka <gymka@archlinux.lt>
2014-06-14 19:50:57 +0300

    infrastuktūros pakeitimas

--- tools/release.sh
+++ tools/release.sh
@@ -2,7 +2,7 @@
 export LC_COLLATE="C"
 file=$1
 if  [ "$1" = "" ]; then
-	file="easylistlithuania.txt"
+	file="../easylistlithuania.txt"
 fi
 
 #src2pipe 
@@ -18,7 +18,7 @@ diff --side-by-side --suppress-common-lines $file ~$file >src2pipe_changes.txt
 rm ~$file
 
 #dėl tarpo meta įspėjimą
-sed -i '/^|/ { s/ /%20/gp; }' easylistlithuania.txt
+sed -i '/^|/ { s/ /%20/gp; }' $file
 
 #surikiuot
 cp $file ~$file
@@ -32,6 +32,6 @@ sed -i '/^\[/d' $file
 sed -i '/^\!/d' $file
 data=$(date +%Y-%m-%d)
 sed -i "1 i\
-\[Adblock Plus 2.0\]\n! Title: Adblock Plus Lithuania\n! Contributors: Mantas Malcius, Algimantas Margevičius\n! About not blocked ad or any issues report to algimantas@margevicius.lt\n! Apie neužblokuotas reklamas ar kitas bėdas praneškite el. paštu algimantas@margevicius.lt\n! Homepage: http://margevicius.lt/easylist_lithuania/\n! Last update: $data\n! Expires: 7 days (update frequency)" $file
+\[Adblock Plus 2.0\]\n! Title: Adblock Plus Lithuania\n! Contributors: Mantas Malcius, Algimantas Margevičius\n! About not blocked ad or any issues report to algimantas@margevicius.lt\n! Apie neužblokuotas reklamas ar kitas bėdas praneškite el. paštu algimantas@margevicius.lt\n! License: this software is licensed under GNU GPL3 license.\n! License: http://www.gnu.org/licenses/gpl-3.0.txt\n! Homepage: http://margevicius.lt/easylist_lithuania/\n! Last update: $data\n!Expires: 7 days (update frequency)" $file
 
 
